6d99218f82aae6603b0cb46beda7fb35e9fe3da8
tomaszkaniecki <kwp@tlen.pl>
2023-10-05 10:59:04 +0200

    szkolenie_angular1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProductsModule,
+    OrdersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
